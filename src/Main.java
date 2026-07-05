8df4297352e3803fc73d7c86d0d71752da6a470e
maniktechlearning-coder <maniktechlearning@gmail.com>
2026-07-05 19:42:24 +0530

    Created the Package and Class for Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -11,5 +12,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+
     }
 }
