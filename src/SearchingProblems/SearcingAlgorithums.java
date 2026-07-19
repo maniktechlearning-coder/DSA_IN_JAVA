55ba02838c2068256f8213d671b1311d333822c5
maniktechlearning-coder <maniktechlearning@gmail.com>
2026-07-19 23:02:35 +0530

    Commiting the Searching algorithum in the class

--- src/SearchingProblems/SearcingAlgorithums.java
+++ src/SearchingProblems/SearcingAlgorithums.java
@@ -102,4 +102,27 @@ public class SearcingAlgorithums {
         }
         return ans;
     }
+
+    public static int searchInsertPosition( int arr[] , int target)
+    {
+        int left = 0;
+        int right = arr.length-1;
+
+        while( left <= right)
+        {
+            int mid = left + (right - left)/2;
+            if( arr[mid] == target)
+            {
+                return mid;
+            }
+            else if (arr[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        return left; //we need to retur the left becaue when the left and right value becomes smae then we need to return left only so
+    }
 }
