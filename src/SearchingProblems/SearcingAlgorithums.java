344c6540d9e6a956dab72c6084f6fca7f4b1a6c8
maniktechlearning-coder <maniktechlearning@gmail.com>
2026-07-19 23:25:59 +0530

    Commiting the Searching algorithum in the class

--- src/SearchingProblems/SearcingAlgorithums.java
+++ src/SearchingProblems/SearcingAlgorithums.java
@@ -140,4 +140,17 @@ public class SearcingAlgorithums {
         // Step 3: Place the first element at the end
         arr[arr.length - 1] = temp;
     }
+
+
+    public static void rotateByRight(int arr[])
+    {
+        int temp = arr[arr.length-1];
+
+        for( int i = arr.length-1 ; i>=0 ; i--)
+        {
+            arr[i] = arr[i-1];
+        }
+
+        arr[0] = temp;
+    }
 }
